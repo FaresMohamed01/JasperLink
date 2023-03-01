d4f6ea33a13e09242d237d4bc1d9b6ab1c93e0cf
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2023-02-28 22:29:54 -0500

    Update SignUpScreen.js

--- SignUpScreen.js
+++ SignUpScreen.js
@@ -1,60 +1,62 @@
 import React from 'react';
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, Button, View, ImageBackground, TouchableOpacity, SafeAreaView, TextInput } from 'react-native';
+import {ScrollView, StyleSheet, Text, Button, View, Image, TouchableOpacity, SafeAreaView, TextInput } from 'react-native';
 import { useFonts } from 'expo-font';
+import {auth} from '../firebase';
+import { getAuth,createUserWithEmailAndPassword } from 'firebase/auth';
 
 const SignUpScreen = () => {
     
-    const [Email, setEmail] = useState('');
-    const [Password, setPassword] = useState();
-    const [password_confirmation, setpassword_confirmation] = useState();
+    const [Email, setEmail] = useState(null);
+    const [Password, setPassword] = useState(null);
+    const [ConfirmPassworrd, setConfirmPassword] = useState('');
 
-    
+    const auth = getAuth()
+
+    //creates a user in firebase
+    const handleSignUp = () => {
+      createUserWithEmailAndPassword(auth, Email, Password)
+      .catch(error => alert(error.message))
+    }
 
     return (
-        <SafeAreaView style = {styles.container}>
-            <ScrollView>
-            <View style = {styles.input}>
 
-              <Text style={styles.text}>
-                Manhattan College Social Network App
-              </Text>
+      <View style = {styles.page}>
+
+          <Image style = {styles.image} source={require('../assets/MC.jpg')} />
+
+          <Text style={styles.title}>
+              Manhattan College Social Network
+          </Text>
               
-              <TextInput
-                style = {styles.email}
-                value = {Email}
-                onChangeText={Email => setEmail(Email)}
-                placeholder = "admin@manhattan.edu"
-              />
+          <TextInput
+              style = {styles.email}
+              value = {Email}
+              onChangeText={Email => setEmail(Email)}
+              placeholder = "admin@manhattan.edu"
+              autoCapitalize="none"
+              autoCorrect={false}
+          />
 
-              <TextInput
-                style = {styles.psw}
-                value = {Password}
-                placeholder = "Password"
-              />
+           <TextInput
+              style = {styles.psw}
+              value = {Password}
+              secureTextEntry
+              onChangeText={Password => setPassword(Password)}
+              placeholder = "Password"
+              autoCapitalize="none"
+              autoCorrect={false}
+           />
+                
+            <TouchableOpacity
+                style = {styles.input}
+                onPress={handleSignUp}
+            >
+            <Text style={styles.Text}>Create Account using MC Email</Text>
+            </ TouchableOpacity>
+       </View>
 
-              <TextInput
-                style = {styles.psw_confirm}
-                value = {password_confirmation}
-                placeholder = "Confirm Password"
-              />
-
-            </View>
-
-            <View style = {styles.button}>
-
-                <Button
-                    style = {styles.input}
-                    title="Create Account using MC Email"
-                    color="black"
-                    fontFamily = "bold"
-                    fontSize = "40"
-                />
-            
-            </View>
-            </ScrollView>
-        </SafeAreaView>
     );
 };
 
@@ -62,42 +64,59 @@ export default SignUpScreen
 
 const styles = StyleSheet.create({
 
-    container: {
-        flex: 1,
-        backgroundColor: 'green',
-        justifyContent: 'center',
-        alignContent: 'center',
-        alignItems: 'center',
-    },
+  page: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#8fbc8f'
 
-    email: {
-        backgroundColor: 'white',
-        height: 30,
-        borderStyle: 'solid',
-        marginBottom: 10,
-        marginTop: 35,
-    },
+  },
 
-    psw: {
-        backgroundColor: 'white',
-        height: 30,
-        borderStyle: 'solid',
-        marginBottom: 10,
-        marginVertical: 35,
-    },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'Noteworthy',
+    position: 'Top',
+    margin: 55,
+    height: 140,
+    borderRadius: 10,
+  },
 
-    psw_confirm: {
-        backgroundColor: 'white',
-        borderStyle: 'solid',
-        height: 30,
-        marginBottom: 10,
-        marginVertical: 35,
-    },
+  email:{
+    fontSize: 25,
+    fontFamily: 'Noteworthy',
+    borderRadius: 100,
+    borderColor: 'black',
+    borderWidth: 1
+  },
 
-    text: {
-        fontFamily: 'Cochin', 
-        fontSize: 30,
-        fontWeight: 'bold',
-    },
+  psw:{
+    fontSize: 25,
+    fontFamily: 'Noteworthy',
+    margin: 25,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 100,
+  },
 
-  });
+  psw_confirm :{
+    fontSize: 25,
+    borderRadius: 100,
+    fontFamily: 'Noteworthy',
+    borderColor: 'black',
+    borderWidth: 1
+
+  },
+
+  Text:{
+    fontSize: 20,
+    fontFamily: 'Noteworthy',
+    margin: 25,
+  },
+
+  image:{
+   height: 95,
+   width: 400,
+  },
+
+});
