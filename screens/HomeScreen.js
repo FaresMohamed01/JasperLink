fd63892d7557f811610ab189044d5d554f30106d
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2023-03-19 19:34:45 -0400

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,68 @@
 import React from 'react';
-import { useContext, useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import {ScrollView, StyleSheet, Text, Button, View, Image, TouchableOpacity, SafeAreaView, TextInput } from 'react-native';
-import { useFonts } from 'expo-font';
-import {auth} from '../firebase';
-import { getAuth,createUserWithEmailAndPassword, signOut } from 'firebase/auth';
-import SignUpScreen from './SignUpScreen';
-import LoginScreen from './LoginScreen';
+import { useState, useEffect } from 'react';
+import {Pressable, FlatList, StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
+import {auth,  db} from '../firebase';
+import { signOut } from 'firebase/auth';
+import { collection, getDocs, query } from "firebase/firestore";
+import {Icon} from 'react-native-elements';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Camera, CameraType } from 'expo-camera';
 
 const HomeScreen = ({navigation}) => {
+  
+  const [posts, setPosts] = useState([]);
+  const refresh = () => window.location.reload()
+  const r_posts= query(collection(db,"posts"));
 
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  const [liked, setLiked] = useState(false);
+
+  const [commented, setCommented] = useState(false);
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+  
+
+  const comments = () => {
+
+
+    <TextInput 
+      placeholder='Enter a comment'
+
+    ></TextInput>
+
+  }
+  const [shared, setShared] = useState(false);
+ 
+
+  useEffect(async()=> {
+
+    const get_posts = await getDocs(r_posts) 
+
+    const posts = []
+
+    get_posts.forEach((doc) => {
+
+      const {Email, image, post, Username, Share, Like, Comment} = doc.data()
+      
+      posts.push ({
+        id: doc.id,
+        Email,
+        image,
+        post,
+        Username,
+        Share,
+        Like,
+        Comment,
+      })
+    })
+    setPosts(posts)
+  },[])       
+   
+    
     const SignOut = () =>  {
 
         signOut(auth)
@@ -20,20 +73,164 @@ const HomeScreen = ({navigation}) => {
         .catch(error => alert(error.message))
     }
 
+
     return (
-        <SafeAreaView >
-            <Text>Email:{auth.currentUser?.email}</Text>
+        
+        <View  style  =  {styles.page}>
             
             <TouchableOpacity
-                onPress={SignOut}
-             >
-                
-                <Text>SignOut</Text>
-
+                style = {styles.input}
+                onPress={() =>navigation.navigate('AddPosts')}
+            >
+            <Text style={styles.Text}>Add Posts</Text>
             </ TouchableOpacity>
-        </SafeAreaView>
+
+            <TouchableOpacity>
+              <Icon
+                style = {styles.input}
+                onPress={() =>navigation.navigate('Profile')}
+            />
+            <Text style={styles.Text}>Profile</Text>
+            </ TouchableOpacity>
+
+            <TouchableOpacity>
+              <Icon
+                style = {styles.input}
+                onPress={() =>navigation.navigate('camera')}
+            />
+            <Text style={styles.Text}>Profile</Text>
+            </ TouchableOpacity>
+
+            <TouchableOpacity
+                style = {styles.input}
+                onPress={SignOut}
+            >
+            <Text style={styles.Text}>SignOut</Text>
+            </ TouchableOpacity>
+
+        
+
+            <FlatList 
+              data = {posts}
+              renderItem = {({item}) => (
+
+              <Pressable>
+                <View>
+
+                  <Text style = {styles.fontStyle}>
+                    {item.Email}
+                    {'\n'}
+                    {item.post}
+                    {'\n'}
+                    {item.Like}
+                    {'\n'}
+
+              
+                    {item.image && <Image source={{ uri: item.image }} style={{ width: 300, height: 900, textAlign: 'center' }} />}
+                    {'\n'}
+                    {'\n'}
+                    
+         
+
+                    
+                  
+                    <Pressable>
+                      <MaterialCommunityIcons
+
+                        name={liked ? "heart" : "heart-outline"}
+                        size={32}
+                        color={liked ? "blue" : "black"}
+                       />        
+                    </Pressable>
+                    
+                  
+                    
+
+                    <Text>{'\t'}</Text>
+
+                    <Pressable style={styles.button}>
+                      <MaterialCommunityIcons
+                        name={"comment-text"}
+                        size={32}
+                       /> 
+
+                           
+                    </Pressable>
+
+                    <Text>{'\t'}</Text>
+
+                    <Pressable style={styles.button}>
+                      <MaterialCommunityIcons
+                        name={"share"}
+                        size={32}
+                       /> 
+
+                           
+                    </Pressable>
+ 
+                 </Text>
+
+                  
+                </View>
+               
+              </Pressable>
+              
+                 )}            
+             />  
+        </View>
     );
 };
 
-export default HomeScreen;
 
+const styles = StyleSheet.create({
+
+    page: {
+        flex: 1,
+        backgroundColor: '#8fbc8f',
+        alignItems: 'center',
+        justifyContent: 'center',
+    
+      },
+    fontStyle: {
+        borderWidth: 4,
+        margin: 20,
+        fontSize: 12,
+        fontFamily: 'Noteworthy',
+        borderColor: 'black',
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+      },
+      comment: {
+        borderWidth: 1,
+        margin: 20,
+        fontSize: 12,
+        fontFamily: 'Noteworthy',
+        borderColor: 'black',
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+      },
+
+      posts:{
+
+        borderWidth: 1,
+
+      },
+
+      button:{
+
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+
+
+      },
+
+    
+  });
+
+export  default HomeScreen;
