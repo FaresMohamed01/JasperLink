1a8fc14ca12cbec681d74f4f731205d5c214dc44
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2023-04-08 00:08:49 -0400

    Update SignUpScreen.js

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,154 +1,190 @@
 import React from 'react';
-import { useContext, useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import {ScrollView, StyleSheet, Text, Button, View, Image, TouchableOpacity, SafeAreaView, TextInput } from 'react-native';
-import { useFonts } from 'expo-font';
-import {auth} from '../firebase';
-import { getAuth,createUserWithEmailAndPassword,sendEmailVerification } from 'firebase/auth';
-import LoginScreen from './LoginScreen';
-import HomeScreen from './HomeScreen';
+import { useState, useEffect } from 'react';
+import {Button,ScrollView, Text, View, Image, TouchableOpacity, TextInput, ImageBackground} from 'react-native';
+import {app, db} from '../firebase';
+import {getAuth,createUserWithEmailAndPassword, sendSignInLinkToEmail, signInWithPopup, FacebookAuthProvider, signInWithRedirect, signInWithEmailLink} from 'firebase/auth';
+import {addDoc, collection} from 'firebase/firestore';
+import { styles } from './Style';
+import { GoogleAuthProvider } from 'firebase/auth';
+
 
 const SignUpScreen = ({navigation}) => {
-    
-    const [Email, setEmail] = useState(null);
-    const [Password, setPassword] = useState(null);
-    const [ConfirmPassworrd, setConfirmPassword] = useState('');
-
-    const auth = getAuth()
-
-    useEffect(() => {
-      const unsubscribe = auth.onAuthStateChanged(user => {
-        if (user) {
-          navigation.navigate("Login")
-        }
-      })
+  const [Email, setEmail] = useState(null);
+  const [first, setFirst] = useState(null);
+  const [last, setLast] = useState(null);
+  const [school, setSchool] = useState(null);
+  const [major, setMajor] = useState(null);
+  const [GPA, setGPA] = useState(null);
+  const [information, setInformation] = useState(null);
+  const [Username, setUsername] = useState(null);
+  const [Password, setPassword] = useState(null);
   
-      return unsubscribe
-    }, [])
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+ 
 
-    //creates a user in firebase
-    const SignUp = ({navigation}) => {
-      createUserWithEmailAndPassword(auth, Email, Password)
-      .catch(error => alert(error.message))
-    }
+  const actionCodeSettings = {
+    url: 'https://capstoneproject-7ce43.firebaseapp.com/__/auth/action?mode=action&oobCode=code',
+    handleCodeInApp: true,
+    dynamicLinkDomain: 'capproject.page.link'
+  };
+  //creates a user in firebase
+  const SignUp = async ({navigation}) => {
 
-    const Emailverified = ({navigation}) =>{
-      sendEmailVerification(auth.currentUser)
-      .then(() => {
-        alert("Please Verify Your Email")
-       })
 
-       .catch(error => alert(error.message))
-    }
+    await createUserWithEmailAndPassword(auth, Email, Password)
 
+    try{
+      addDoc(collection(db,`users`),{
+          
+          Email: auth.currentUser?.email,
+          first: first,
+          last: last,
+          school: school,
+          major:  major,
+          GPA: GPA,
+          information: information,
+          Username: Username,
+          Password: Password
+        });
+
+        setEmail('');
+        setFirst('');
+        setLast('');
+        setSchool('');
+        setMajor('');
+        setGPA('');
+        setInformation('');
+        setUsername('');
+        setPassword('');
+
+        alert("Account Created!")
+        
+      
+        }
+        catch(error){
+          alert("Enter a valid email")
+        }
+         
+          
+
+
+  }
+
+  
     return (
 
-      <View style = {styles.page}>
+      <View style = {styles.container}>
+        <ImageBackground style = {styles.background_image} source={require('../assets/background.jpg')}>
+        <ScrollView>
 
-          <Image style={styles.image}
-              source={require('../assets/ManhattanLogo.png')}>
-          </Image>
-              
-          <TextInput
-              style = {styles.email}
-              value = {Email}
-              onChangeText={Email => setEmail(Email)}
-              placeholder = "admin@manhattan.edu"
-              autoCapitalize="none"
-              keyboardType="email-address"
-              autoCorrect={false}
-          />
-
-           <TextInput
-              style = {styles.psw}
-              value = {Password}
-              secureTextEntry
-              onChangeText={Password => setPassword(Password)}
-              placeholder = "Password"
-              autoCapitalize="none"
-              autoCorrect={false}
-           />
-                
-            <TouchableOpacity
-                style = {styles.input}
-                Emailverified
-                onPress={SignUp}
-            >
-            <Text style={styles.Text}>Create Account using MC Email</Text>
-            
-            </ TouchableOpacity>
-
-            <TouchableOpacity
-                style = {styles.input}
-              //  onPress={handleSignUp}
-            >
-            <Text style={styles.Text}>Forget Password</Text>
-            </ TouchableOpacity>
-
-            <TouchableOpacity
-                style = {styles.input}
-                onPress={() =>navigation.navigate('Login')}
-            >
-            <Text style={styles.Text}>Login</Text>
-            </ TouchableOpacity>
-
-       </View>
-
-    );
-};
-
-export default SignUpScreen
-
-const styles = StyleSheet.create({
-
-  page: {
-    flex: 1,
-    backgroundColor: '#8fbc8f',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
+        <View style={styles.signup_border}>
+           
+           <Text style={styles.signup_text}> Create Account </Text>
+ 
+           <Text style={styles.email}> Username </Text>
+ 
+           <View style={styles.email_border}>
 
 
-  email:{
-    margin: 10,
-    borderWidth: 3,
-    padding: 10,
-    width:275,
-    borderRadius:10,
-    fontSize: 25,
-    fontFamily: 'Noteworthy',
-    borderColor: 'black',
-  },
+              <TextInput 
+               style = {styles.email_text}
+               value = {Username}
+               onChangeText={Username => setUsername(Username)}
+               placeholder = " John Doe"
+               autoCapitalize="none"
+               autoCorrect={false}
+             />
 
-  psw:{
-    fontSize: 25,
-    fontFamily: 'Noteworthy',
-    borderColor: 'black',
-    margin: 40,
-    borderWidth: 3,
-    padding: 10,
-    width:275,
-    borderRadius:10
-  },
+              <Image style={styles.email_icon}
+               source={require('../assets/email_image.png')}>
+              </Image>
 
-  psw_confirm :{
-    fontSize: 25,
-    borderRadius: 100,
-    fontFamily: 'Noteworthy',
-    borderColor: 'black',
-    borderWidth: 1
+              </View>
 
-  },
+              <Text style={styles.email}> Email </Text>
+ 
+              <View style={styles.email_border}>
 
-  Text:{
-    fontSize: 20,
-    fontFamily: 'Noteworthy',
-    margin: 25,
-  },
+              <TextInput 
+               style = {styles.email_text}
+               value = {Email}
+               onChangeText={Email => setEmail(Email)}
+               placeholder = " example@manhattan.edu"
+               autoCapitalize="none"
+               autoCorrect={false}
+             />
 
-  image:{
-    margin: 50,
-  },
+             
+             <Image style={styles.email_icon}
+               source={require('../assets/email_image.png')}>
+             </Image>
+ 
+           </View>
+ 
+           <Text style={styles.email}> Password </Text>
+ 
+           <View style={styles.email_border}>
+ 
+             <TextInput 
+               style = {styles.password_text}
+               value = {Password}
+               secureTextEntry
+               onChangeText={Password => setPassword(Password)}
+               placeholder = "Password"
+               autoCapitalize="none"
+               autoCorrect={false}
+             />
+             
+             <Image style={styles.password_icon}
+               source={require('../assets/password_icon.png')}>
+             </Image>
+             
+           </View>
+ 
+           <Text style={styles.have_account_text}>Already have an account?</Text>
+ 
+           <TouchableOpacity
+                 onPress={() =>navigation.navigate('Login')}
+             >
+             <Text style={styles.sign_in_button}>Sign In</Text>
+ 
+           </ TouchableOpacity>
+ 
+           <View style={styles.sign_up_button_border}>
+             <TouchableOpacity 
+               style = {styles.input}              
+               onPress = {SignUp}
 
-});
+             >
+ 
+               <Text style={styles.sign_up_button}>SIGN UP</Text>
+             
+             </ TouchableOpacity>
+
+
+
+        
+           </View>
+
+  
+           
+ 
+        </View>
+       
+        </ScrollView>
+        </ImageBackground>
+      </View>
+    )
+}
+    
+  
+      
+   
+   
+  
+
+ 
+
+
+export default SignUpScreen;
