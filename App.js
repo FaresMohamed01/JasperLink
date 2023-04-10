d0f6f81a5381358f21525a8526dc3e8ba71c3788
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2023-04-10 11:41:47 -0400

    Update App.js

--- App.js
+++ App.js
@@ -6,6 +6,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignUpScreen from './screens/SignUpScreen';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import AddPosts from './screens/AddPosts';
+import Profile from './screens/Profile';
+import NavBar from './modules/NavBar';
+import Comments from './screens/Comments';
+import {firebase} from './firebase';
+import PostCard from './modules/Posts';
+import Messaging from './screens/DirectMessage';
+import Settings from './screens/Settings';
+import ChatRoom from './screens/ChatRoom';
+import FullProfile from './screens/FullProfile';
 
 
 const Stack = createNativeStackNavigator();
@@ -16,19 +26,35 @@ const App = ({navigation}) => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen options={{
-          headerLeft: () => (
-            <Button
-              title="Info"
-              color="#fff"
-            /> 
-            ), 
+          headerShown: false
             }} 
             name="SignUp" component={SignUpScreen} 
             />
         
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Login" options={{
+          headerShown: false
+            }}  component={LoginScreen} />
+
+        <Stack.Screen name = "NavBar" options={{
+          headerShown: false
+            }} component={NavBar}/>
         
-        <Stack.Screen name="Home" component={HomeScreen} />         
+        <Stack.Screen name="Home"component={HomeScreen} />    
+
+        <Stack.Screen name="AddPosts"options={{
+           
+            }}  component={AddPosts} />     
+
+        <Stack.Screen name="Profile" component={Profile} />
+
+        <Stack.Screen name="Posts" component={PostCard} />  
+
+        <Stack.Screen name="comments" component={Comments} />  
+        <Stack.Screen name="messaging" component={Messaging} />  
+        <Stack.Screen name="Settings" component={Settings} />  
+        <Stack.Screen name="ChatRoom" component={ChatRoom} /> 
+        <Stack.Screen name="FullProfile" component={FullProfile} /> 
+ 
       </Stack.Navigator>
 
     </NavigationContainer>
