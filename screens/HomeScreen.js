a7e79af00f6eb5b7b7d7624c7aa187018b8f5421
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2023-04-15 23:53:35 -0400

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,307 +1,149 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import {Pressable, FlatList, StyleSheet, Text, View, Image, TouchableOpacity, TextInput, RefreshControl, ActivityIndicator, Button, ScrollView} from 'react-native';
-import {auth,  db, app} from '../firebase';
-import { signOut } from 'firebase/auth';
-import {Icon} from 'react-native-elements';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Camera, CameraType } from 'expo-camera';
-import NavBar from './navbar';
-import {addDoc, collection, doc, getDoc, getDocFromCache, getDocs, where, query, QuerySnapshot, updateDoc, setDoc, documentId, collectionGroup, DocumentReference, DocumentSnapshot, orderBy, arrayUnion, arrayRemove} from 'firebase/firestore';
-import { ref } from 'firebase/storage';
-import ButtonNavBar from '../compnents/ButtonNavBar';
-import PostCard from '../compnents/PostCard';
+//Home Screen Page
+import React, { useState, useEffect }  from 'react';
+import {Pressable, FlatList, Text, View, Image, ActivityIndicator, RefreshControl} from 'react-native';
+import {db} from '../firebase';
+import {collection, getDocs, where, query, collectionGroup, orderBy} from 'firebase/firestore';
+import ButtonNavBar from '../modules/NavBar';
+import TopBanner from '../modules/TopBanner';
+import PostCard from '../modules/Posts';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { styles } from './Style';
 
-const HomeScreen = ({navigation}) => {
-  
+const HomeScreen = ({navigation, user}) => {
+
+  //Posts Array and Post query 
   const [posts, setPosts] = useState([]);
+  const posts_query = query (collectionGroup (db, 'posts'), orderBy('timestamp','desc'));
+
+  //Comments Array and Comment query 
   const [comment, setcomment] = useState([]);
-  const refresh = () => window.location.reload()
+  const comments_query = query (collectionGroup (db, 'comments'));
 
-  const [liked, setliked] = useState(false)
-  const [share, setshare] = useState([])
+  //Refresh the page
+  const [refresh, setRefresh] = useState(true);
 
-  const [type, setType] = useState(CameraType.back);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
 
-  const [refreshing, setRefreshing] = useState(true);
-  const [userData, setUserData] = useState([]);
+  useEffect(()=> {
+    fetchData()
+    fetchComments()
+  },[]) 
 
-  const r_posts= query (collectionGroup (db, 'posts'), orderBy('timestamp','desc'));
-  const c_comments = query (collectionGroup (db, 'comments'));
+  const fetchData = async() => {
+      //function to fetch posts
+        const posts = []
+        const snapshot = await getDocs(posts_query) 
 
-useEffect(()=> {
-
-  async function  fetchData(){
-
-  const snapshot = await getDocs(r_posts) 
-
-  const posts = []
-
-  posts.forEach((doc) => {
-    const {Email, image, post, Username} = doc.data()
+        setRefresh(false);
     
-    posts.push ({
-      id: doc.id,
-      Email,
-      image,
-      post,
-      Username,
-    })
-  })
-
-  setPosts(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data()})))
-}
-
-async function  fetchComments(){
-
-  const snapshot = await getDocs(c_comments) 
-
-  const comment = []
-
-  comment.forEach((doc) => {
-    const {Email, post, comment} = doc.data()
-    
-    comment.push ({
-      id: doc.id,
-      Email,
-      post: doc(collection(db,"posts"), where ("post", "==", post)),
-      comment,
-    })
-  })
-
-  setcomment(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data()})))
-}
-fetchData()
-fetchComments()
-
-},[]) 
-
-  //save likes in database
-
-  //save comments in database
-
-  //show comments in drop menu
-
-  //save shares in database
-
-   
-
-    
-  const SignOut = () =>  {
-
-        signOut(auth)
-        .then(() => {
-            navigation.navigate("Login")
+        posts.forEach((doc) => {
+          const {Email, image, post, Username} = doc.data()
+        
+          posts.push ({
+            id: doc.id,
+            Email,
+            image,
+            post,
+            Username,
+          })
         })
     
-        .catch(error => alert(error.message))
+        setPosts(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data()})))
   }
 
+  const fetchComments = async () => {
+    const comment = []
+    const snapshot = await getDocs(comments_query) 
+
+    comment.forEach((doc) => {
+      const {Email, post, comment} = doc.data()
+  
+      comment.push ({
+        id: doc.id,
+        Email,
+        post: doc(collection(db,"posts"), where ("post", "==", post)),
+        comment,
+     })
+    })
+
+      setcomment(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data()})))
+
+  }
 
   return (
-        
-        <View  style  =  {styles.page}>
-          <SafeAreaView>
-
-        
-          <TouchableOpacity
-                 onPress={() =>navigation.navigate('Login')}
-             >
-             <Text>Sign Out</Text>
- 
-           </ TouchableOpacity>
-<FlatList 
-data = {posts}
-
-renderItem = {({item}) => (
-  
-<Pressable>
-
-  <View>
-        <Text style = {styles.fontStyle}>
-      
-      {item.Email}
-
-      {'\n'}
-      {item.post}
-      {'\n'}
-
-      {'\n'} {item.image && <Image source={{uri:item.image}} style={styles.image} />}
-
-      {'\n'} {'\n'} {'\n'} 
-
-          <PostCard navigation={navigation}/>
-
-        <FlatList
-
-        
-        data = {comment}
-
+    <View  style = {styles.page}>
+       {refresh ? <ActivityIndicator/> : null}
+      <SafeAreaView style = {styles.flatlist}>
+      <TopBanner/>
+      <FlatList 
+        data = {posts}
         renderItem = {({item}) => (
-          
-        
-        <Pressable>
-        
-          <View>
-          <Text style = {styles.font}>
-        
-            {'\n'}{item.Email}: {item.comment}
-              </Text>
+          <Pressable>
+            <View>
+              <Text style = {styles.fontStyle}>
+                <View>
+                  <Image style={styles.profile_icon}
+                    source={require('../assets/email_image.png')}>
+                  </Image>
+                  
+                  <Text style={styles.private_email}>
+                      {item.Email}
+                  </Text>
             
+            </View> 
+
+      {'\n\n'}{item.image && <Image source={{uri:item.image}} style={styles.image} />}
+      
+            <View>
+              <Text style = {styles.like_text}>Like</Text>
+              <Text style = {styles.comment_text}>Comment</Text>
+              <Text style = {styles.share_text}>Share</Text>
+            </View>
+
+            <PostCard navigation={navigation} />
+      
+            <FlatList 
+              data = {comment}  
+              renderItem = {({item}) => (
+                 <Pressable>
+                  <View>
+                    <Text style = {styles.font}>
+                      {'\n'}{item.Email}: {item.comment}
+                    </Text>
+            
+                  </View>
+                </Pressable>
+
+               )}
+               
+             />
+       
+        
+        {'\n'}<View>
+                <Text style = {styles.abstract}>Abstract: </Text>
+                  <Text style = {styles.users_post}>
+                    {item.post}
+                  </Text>
+              </View>
+        
+            </Text>
           </View>
-        </Pressable>
-
-        )}
-        />
-            {'\n'}   
-      </Text>
-
-    
-  </View>
  
-</Pressable>
+        </Pressable>
+     )}  
+     refreshControl={
+     <RefreshControl refreshing={refresh} onRefresh={fetchData} />
+     }
+    />
 
-   )}  
-        
-/>
-
-
-
-
-            <View style = {styles.nav}>
-
-            <ButtonNavBar navigation={navigation}/>
-
-
-           
-           
-            </View>  
-                       
-            </SafeAreaView>  
-        </View>
-        
+      <View style = {styles.nav}>
+        <ButtonNavBar navigation={navigation}/>
+      </View>  
+      </SafeAreaView>   
+    </View>  
   );
+
 };
 
-
-const styles = StyleSheet.create({
-
-    page: {
-        flex: 1,
-        backgroundColor: '#8fbc8f',
-        alignItems: 'center',
-        justifyContent: 'center',
-    
-      },
-    fontStyle: {
-        borderWidth: 1,
-        margin: 20,
-        fontSize: 12,
-        fontFamily: 'Noteworthy',
-        borderColor: 'black',
-        textAlign: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-
-      },
-
-      font:{
-
-        textAlign: 'left',
-        padding: 40,
-        alignSelf:'flex-start',
-        padding: 8,
-
-      },
-      comment: {
-        borderWidth: 1,
-        fontSize: 12,
-        fontFamily: 'Noteworthy',
-        borderColor: 'black',
-        textAlign: 'flex-right',
-        justifyContent: 'center',
-        alignItems: 'center',
-
-      },
-
-      posts:{
-
-        borderWidth: 1,
-
-      },
-
-      button:{
-
-        textAlign: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-
-
-
-      },
-
-      profile:{
-
-        marginTop: -2,
-        marginBottom: 22,
-        marginLeft: 340,
-        marginRight: 10
-      
-      },
-
-      home:{
-
-        marginRight: 340,
-        marginBottom: -39,
-        marginLeft: 13,
-      },
-
-      Search:{
-
-        marginRight: 26,
-        marginBottom: 35,
-        marginLeft: 270,
-        marginTop: -60
-
-      },
-
-
-
-      navigation:{
-
-        marginRight: 20,
-        marginBottom: -39,
-        marginLeft: 90,
-     
-
-
-      },
-
-      Post :{
-
-        marginRight: 20,
-        marginBottom: -39,
-        marginLeft: 180,
-
-      },
-
-      nav:{
-        backgroundColor: `#ffebcd`
-      },
-      image:{
-        width: 350, 
-        height: 300,
-        display: 'flex',
-        justifyContent: 'center',
-        
-      }
-
-
-    
-});
-
-export  default HomeScreen;
+export default HomeScreen;
 
 
