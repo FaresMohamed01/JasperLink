b305650837fa456d2e718839ff4074609b064731
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2023-04-24 13:40:55 -0400

    Create InfiniteHits.js

---
+++ screens/InfiniteHits.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, FlatList, TouchableOpacity, Text} from 'react-native';
+import PropTypes from 'prop-types';
+import { connectInfiniteHits } from 'react-instantsearch-native';
+import { styles } from '../Style';
+import { useNavigation } from '@react-navigation/native';
+
+
+const InfiniteHits = ({ hits, hasMore, refineNext, route, navigation}) => {
+  
+    navigation = useNavigation()
+
+    return (
+      //Create A FlatList with all information. item.objectID is fetched from the Algolia database.
+      // Afterwards, the username are displayed in the search page and each username are pressable to the SearchProfiles.js
+      <FlatList
+            data={hits}
+            keyExtractor={item => item.objectID}
+            ItemSeparatorComponent={() => <View style={styles.page} />}
+            onEndReached={() => hasMore && refineNext()}
+            renderItem={({ item }) => (
+                  
+                <View style={styles.item}>
+      
+      <TouchableOpacity
+          onPress={() =>navigation.navigate('SearchProfiles')}
+      >
+       
+       <Text>itemId: {item.Username}</Text> 
+       
+               </TouchableOpacity>
+                </View>
+            )}
+        />
+    )
+    
+}
+//PropTypes for hits, hasmore, refinenext.
+InfiniteHits.propTypes = {
+  hits: PropTypes.arrayOf(PropTypes.object).isRequired,
+  hasMore: PropTypes.bool.isRequired,
+  refineNext: PropTypes.func.isRequired,
+};
+
+export default connectInfiniteHits (InfiniteHits);
