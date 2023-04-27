507fccbc133f19da581a6082c639098c7c50b33f
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2023-04-27 12:36:49 -0400

    Update ChatRoom.js

--- screens/ChatRoom.js
+++ screens/ChatRoom.js
@@ -1,69 +1,71 @@
 import { StyleSheet } from 'react-native'
-import React , {useCallback,useLayoutEffect,useEffect,useState, useId}from 'react'
+import React , {useCallback,useLayoutEffect,useEffect,useState, useRef}from 'react'
 import { GiftedChat, Bubble, SystemMessage, IMessage, Send, SendProps } from 'react-native-gifted-chat'
 import {collection, addDoc, orderBy, query, onSnapshot, where, getDocs, serverTimestamp} from 'firebase/firestore';
 import {auth,db} from '../firebase';
 import { styles } from '../Style';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 
 const ChatRoom = ({user, route}) => {
     const [messages, setMessages] = useState([]);
     const [posts, setPosts] = useState(null)
     const [users, setUsers] = useState(null)
     const r_posts= query (collection (db, 'users'), where("Email","!=",auth.currentUser?.email));
-    const p_posts= query (collection (db, 'chats'));
+    
+    useLayoutEffect(() => {
+      const collectionRef = collection(db, "chats");
+      const q = query(collectionRef , orderBy("createdAt", "desc"));
+  
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        console.log("querySnapshot unsusbscribe");
+        setMessages(
+          querySnapshot.docs.map((doc) => ({
+            _id: doc.data()._id,
+            createdAt: doc.data().createdAt.toDate(),
+            text: doc.data().text,
+            user: doc.data().user,
+          }))
+        );
+      });
+      return unsubscribe;
+    }, []);
 
-    useEffect(() => {
-      setMessages([
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-      ])
-    }, [])
-
-    const onSend = (messageArray) => {
-       const message = messageArray[0]
-       
-       const usermsg = {
-        ...message,
-        sentBy: auth.currentUser?.email,
-        sentTo: r_posts,
-        createdAt: new Date()
-      }
-
-      setMessages(previousMessages => GiftedChat.append(previousMessages, usermsg))
-      
-      addDoc(collection(db,`users/${auth.currentUser?.email}/`,"chats"),{
-        usermsg,
-        text: messages[0].text,
-        user: auth.currentUser?.email,
-
-    })
-      
-    }
+    const onSend = useCallback((messages = []) => {
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, messages)
+      );
+      // setMessages([...messages, ...messages]);
+      const { _id, createdAt, text, user } = messages[0];
+      addDoc(collection(db, "chats"), {
+        _id,
+        createdAt,
+        text,
+        user,
+      });
+    }, []);
 
 
     return (
     
-      <GiftedChat 
-      style={{flex: 1}}
+      <GiftedChat
       messages={messages}
-      onSend={messages=> onSend(messages)}
-      user={{ 
-        _id: auth.currentUser?.email
+      showAvatarForEveryMessage={false}
+      showUserAvatar={false}
+      onSend={(messages) => onSend(messages)}
+      messagesContainerStyle={{
+        backgroundColor: "#fff",
       }}
-       />
-     
-     
-
-        
-    )
-}
+      textInputStyle={{
+        backgroundColor: "#fff",
+        borderRadius: 20,
+      }}
+      user={{
+        _id: auth?.currentUser?.email,
+      }}
+    />
+  );
+};
 
 export default ChatRoom;
+
