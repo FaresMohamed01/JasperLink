6a90d8327e03a93815477621f34b4bc65a547c6e
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2023-04-30 17:19:36 -0400

    Update SignUpScreen.js

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -2,43 +2,42 @@
 import React from 'react';
 import { useState} from 'react';
 import {Text, View, Image, TouchableOpacity, TextInput, ImageBackground} from 'react-native';
-import {db} from '../firebase';
-import {getAuth,createUserWithEmailAndPassword} from 'firebase/auth';
+import {firebase, db} from '../firebase';
 import {setDoc, doc} from 'firebase/firestore';
+import {getAuth} from 'firebase/auth';
 import { styles } from '../Style';
 import TopHeaderBar from '../modules/TopHeaderBar';
 import redx from '../assets/Red_x.png'
 
-const SignUpScreen = ({route, navigation}) => {
-
+const SignUpScreen = ({navigation}) => {
   //Needed information for sign up and user profile
   const [Email, setEmail] = useState(null);
-  const [first, setFirst] = useState(null);
-  const [last, setLast] = useState(null);
-  const [major, setMajor] = useState(null);
-  const [Username, setUsername] = useState(null);
+  const [Name, setName] = useState(null);
   const [Password, setPassword] = useState(null);
   const [confirmPassword, setConfirmPassword] = useState(null);
-  const [image, setImage] = useState(null)
-  const [uploading, setUploading] = useState(false)
 
-  //empty fields
+  const [major, setMajor] = useState(null);
+  const [image, setImage] = useState(null)
   const [school, setSchool] = useState(null);
   const [GPA, setGPA] = useState(null);
   const [information, setInformation] = useState(null);
-  const [Bookmarks, setBookmarks] = useState([]);
-  const [Friends, setFriends] = useState([]);
-
-  const auth = getAuth();
-
+  
   //Pattern to check Email is @manhattan.edu
   const pattern = new RegExp("^[a-zA-Z0-9+_.-]+@manhattan.edu$");
-    const [EmailisValid, setEmailIsValid] = useState(true);
-    const [PasswordisValid, setPasswordIsValid] = useState(true);
-     
+  const [EmailisValid, setEmailIsValid] = useState(true);
+  const [PasswordisValid, setPasswordIsValid] = useState(true);
+  
+  const auth = getAuth();
 
+  //To get firebase url information for API Call
+  const actionCodeSettings = {
+    url: 'https://capstoneproject-7ce43.firebaseapp.com/__/auth/action?mode=action&oobCode=code',
+    handleCodeInApp: true,
+    dynamicLinkDomain: 'capproject.page.link'
+  };
+     
   //Specifically checking for passwords
-  const validCheck = () => {
+  function validCheck (Password) {
     if(Password != confirmPassword){
       setPasswordIsValid(false)
     }
@@ -48,50 +47,60 @@ const SignUpScreen = ({route, navigation}) => {
 
   }
 
-//Specifically checking for emails
-  const validateEmail = (Email) => {
+  //Specifically checking for emails
+  function validateEmail(Email) {
     if (pattern.test(Email)) {
         setEmailIsValid(true);
     } else{
         setEmailIsValid(false);
     }
-}
-
-  
-  //creates a user in firebase
-  const SignUp = async ({navigation}) => {
-   try {
-     await createUserWithEmailAndPassword(auth,Email, Password)
-
-     setDoc(doc(db,`users`,`${auth.currentUser?.email}`),{
-      Email: auth.currentUser?.email,
-      first: first,
-      last: last,
-      school: school,
-      major:  major,
-      GPA: GPA,
-      information: information,
-      Username: Username,
-      Password: Password,
-      Bookmarks: Bookmarks,
-      Friends: Friends,
-      image: image
-    });
-        alert("Account Created!")
-      }
-
-   
-    catch(error){
-      alert ("Error! Check your Information")
-    }
   }
 
+  //creates a user in firebase
+  const SignUp = async () => {
+
+    if (pattern.test(Email) && Password == confirmPassword){
+      await firebase.auth().createUserWithEmailAndPassword(Email, Password)
+
+      .then (() => {
+        firebase.auth().currentUser.sendEmailVerification(actionCodeSettings)
+        .then(() => {
+          alert ('Verify Your Email!')
+        }).catch((error) => {
+          alert("Error")
+        })
+        const loggedin = auth.beforeAuthStateChanged(user => {
+          if (user.emailVerified) {
+          setDoc(doc(db,`users`,`${auth.currentUser?.email}`),{
+            Email: auth.currentUser?.email,
+            Name: Name,
+            Password: Password,
+            school: school,
+            major:  major,
+            GPA: GPA,
+            information: information,
+            image: image
+          })
+        }
+        else {
+          alert("Error")
+        }
+      })
+      })
+      .catch((error) => {
+        alert("Email already Exists!")
+      })
+    }
+    else{
+      alert ("Check your email and password!")
+    }
+  }
+  
   //Return all information on signup page
   return(
     <View style = {styles.container}>
-        
         <ImageBackground style = {styles.background_image} source={require('../assets/background.jpg')}>
-          
+
         <View>
             <TopHeaderBar navigation={navigation}/>
         </View>
@@ -100,14 +109,14 @@ const SignUpScreen = ({route, navigation}) => {
            
            <Text style={styles.signup_text}> Create Account </Text>
  
-           <Text style={styles.signup_email}> Username </Text>
+           <Text style={styles.signup_email}> Full Name</Text>
  
            <View style={styles.signup_email_border}>
 
               <TextInput 
                style = {styles.signup_email_text}
-               value = {Username}
-               onChangeText={Username => setUsername(Username)}
+               value = {Name}
+               onChangeText={Name => setName(Name)}
                placeholder = " John Doe"
                autoCapitalize="none"
                autoCorrect={false}
@@ -117,15 +126,15 @@ const SignUpScreen = ({route, navigation}) => {
                source={require('../assets/email_image.png')}>
               </Image>
 
-          </View>
+           </View>
 
-          <Text style={styles.login_email}> Email </Text>
+           <Text style={styles.login_email}> Email </Text>
 
             <View style={styles.login_email_border}>
 
                 <TextInput 
                     style = {styles.login_email_text}
-                    placeholder="Email@manhattan.edu"
+                    placeholder="jdoe@manhattan.edu"
                     autoCapitalize="none"
                     autoCorrect={false}
                     onChangeText={(Email) => {setEmail(Email); validateEmail(Email)}}
@@ -145,6 +154,7 @@ const SignUpScreen = ({route, navigation}) => {
             </View>
  
            <Text style={styles.signup_email}> Password </Text>
+
            <View style={styles.signup_email_border}>
              <TextInput 
                style = {styles.signup_password_text}
@@ -193,8 +203,8 @@ const SignUpScreen = ({route, navigation}) => {
            <Text style={styles.have_account_text}>Already have an account?</Text>
  
            <TouchableOpacity
-                 onPress={() =>navigation.navigate('Login')}
-             >
+                onPress={() =>navigation.navigate('Login')}
+            >
              <Text style={styles.sign_in_button}>Sign In</Text>
  
            </ TouchableOpacity>
@@ -209,10 +219,10 @@ const SignUpScreen = ({route, navigation}) => {
              </ TouchableOpacity>
 
            </View>
-        </View>
-      </ImageBackground>
-     </View>
-    )
-  }
+         </View>
+       </ImageBackground>
+    </View>
+  )
+}
 
 export default SignUpScreen;
