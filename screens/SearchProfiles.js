9bf52733313ee62edd57acae8be4f046cd69a3b0
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2023-04-30 17:21:21 -0400

    Update SearchProfiles.js

--- screens/SearchProfiles.js
+++ screens/SearchProfiles.js
@@ -1,161 +1,224 @@
-import { StyleSheet, Text, View, FlatList,  Image, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, FlatList,  Image, TouchableOpacity, TouchableHighlight } from 'react-native'
 import React, { useState, useEffect } from 'react'
-import { query, collection, where, getDocs} from 'firebase/firestore';
+import { query, collection, collectionGroup, where, getDocs, setDoc, addDoc, updateDoc, doc} from 'firebase/firestore';
 import { auth, db} from '../firebase';
 import { styles } from '../Style';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Divider } from 'react-native-elements';
 import TopHeaderBar from '../modules/TopHeaderBar';
 import ButtonNavBar from '../modules/NavBar';
-import InfiniteHits from './InfiniteHits';
-import { object, objectOf } from 'prop-types';
-import algoliasearch from 'algoliasearch';
-import { CurrentRefinements, Hits } from 'react-instantsearch-hooks-web';
-import {firebase} from '../firebase'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Friends from './Friends';
 
-const SearchProfiles = ({navigation, route}) => {
 
-    const [users, setUsers] = useState('');
-    const user = query(collection(db, "users"), (where ("Email", "==", auth.currentUser?.email)));
-  
-  
-    useEffect(()=> {
-  
-    async function document(){
-  
-      const get_user = await getDocs(user) 
-  
-      const users = []
-      get_user.forEach((doc) => {
-  
-        const { Email, Password, Username, GPA, first, last, information, major, school } = doc.data()
-  
-        users.push ({
-          id: doc.id,
-          Email,
-          Password,
-          Username,
-          GPA,
-          first,
-          last,
-          information,
-          major,
-          school,
-        })
-  
-      })
-  
-      setUsers(users)
-    }
-     
+const SearchProfiles = ({route,navigation}) => {
+  const { itemId } = route.params;
+  const { itemEmail } = route.params;
+  const { otherParam } = route.params;
+  const [friends_array, setFriends_Array] = useState([]);
+  const [users, setUsers] = useState([]);
+  const user = query(collection(db,`users`),  where ("Email","==", itemEmail));
+
+  const friend_query= query(collectionGroup(db,`friends`));
+
+  async function document(){
+
+    const get_user = await getDocs(user) 
+
+    const users = []
    
+    get_user.forEach((doc) => {
+
+      const { Email, Password, Name, GPA, first, last, information, major, school, image } = doc.data()
+
+      users.push ({
+        id: doc.id,
+        Email,
+        Password,
+        Name,
+        GPA,
+        first,
+        last,
+        information,
+        major,
+        school,
+        image
+      })
+
+    })
+
+    setUsers(get_user.docs.map((doc) => ({ id: doc.id, ...doc.data()})))
+  }
+
+  async function friend_check(itemEmail){
+
+    const get_user = await getDocs(friend_query) 
+
+    const friends_array = []
+   
+    get_user.forEach((doc) => {
+
+      const { Email, Email2 } = doc.data()
+
+      friends_array.push ({
+        id: doc.id,
+        Email,
+        Email2,
+      })
+
+    })
+
+    setFriends_Array(get_user.docs.map((doc) => ({ id: doc.id, ...doc.data()})))
+  }
+
+  useEffect (() => {
     document()
-    },[])
+    friend_check(itemEmail)
+
+  },[])
+
+  const addfriends = () => {
+    if (auth.currentUser?.email != itemEmail && itemEmail != friend_query) {
+      addDoc(collection(db,`friends/${auth.currentUser?.email}`, "friends"),{
+        Email: auth.currentUser?.email,
+        Email2: itemEmail
+      });
+      alert("Friend Added!");
+    }
+    else {
+      alert("Error!");
+    }
+
+  }
+
+  const addfollows = () => {
+
+    try {
+      addDoc(collection(db,`follows/${itemEmail}/`,"follows"),{
+        Email: auth.currentUser?.email,
+        Email2: itemEmail
+      });
+      alert("Followed!");
+    }
+    catch (e) {
+      alert("Information Missing!");
+    }
+
+  }
+
+  return (
+    <View style =  {styles.page}>
+
+        <TopHeaderBar navigation={navigation}/>
+
+        <SafeAreaView style = {styles.flatlist}>
+
+       <FlatList
+        data = {users}
+        renderItem = {({item}) => (
+            <View>
+                <Image source={{uri:item.image}} style={styles.users_image} />
+           
+                <Text>{'\n'}{'\n'}</Text>
+            
+                <Text style = {styles.profile_full_name}>
+                    {item.Name}
+                </Text>
+
+                <TouchableOpacity onPress={addfriends} style = {styles.friends}>
+          <MaterialCommunityIcons
   
-    return (
-      <View style =  {styles.page}>
+            name={'account-plus'}
+            color = "green"
+            size={60}
+            
+            />        
+          <Text style = {styles.friend}> Friend </Text>   
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={addfollows} style = {styles.follows}>
+                  <MaterialCommunityIcons
   
-          <TopHeaderBar navigation={navigation}/>
+                    name={'account-check'}
+                    color = 'green'
+                    size={60}
+                  />    
+
+                  <Text style = {styles.follow}> Follow </Text>   
   
-          <SafeAreaView style = {styles.flatlist}>
-  
-         <FlatList
-  
-          data = {users}
-          renderItem = {({item}) => (
-              <View>
+                </TouchableOpacity>
+        
+                
+                <Text style = {styles.hold_profile_info}>
                   
-              
-                  <Image style={styles.users_image}
-                      source={require('../assets/email_image.png')}>
-                  </Image>
-             
-  
-                  <Text>{'\n'}{'\n'}{'\n'}</Text>
-              
-             
-                  <Text style = {styles.profile_full_name}>
-                      {item.first} {item.last}
-                  </Text>
-             
-              
-                  
-                  <Text style = {styles.hold_profile_info}>
-                    
-                  </Text>
-  
-                  
-                  <Text style = {styles.personal_info_text}>Personal Info</Text>
-                  
-  
-                  <View style = {styles.profile_border1} >
-                      <Text >
-                          <Text>{'\n'}</Text>
-                            
-                          <Text style = {styles.first_name_text} >   First Name: </Text> 
-                          <Text style = {styles.first_name} >{item.first}</Text>
-  
-                          <Text>{'\n'}{'\n'}</Text>
-  
-                          <Text style = {styles.last_name_text} >   Last Name: </Text>
-                          <Text style = {styles.last_name} >{item.last}</Text>
-  
-                          <Text>{'\n'}{'\n'}</Text>
-  
-                          <Text style = {styles.users_email_text}>   Email: </Text>
-                          <Text style = {styles.users_email} >{item.Email}</Text> 
-  
-                          <Text>{'\n'}{'\n'}</Text>
-  
-                          <Text style = {styles.username_text} >   Username: </Text>
-                          <Text style = {styles.username} >{item.Username}</Text>
-  
-                          <Text>{'\n'}</Text>
-                      </Text>
-                  </View>
-  
-                  <Text style = {styles.about_me_text}>About Me</Text>
-  
-                  <View style = {styles.profile_border2} >
-                      <Text >
-                          <Text>{'\n'}</Text>
-  
-                          <Text style = {styles.school_text} >   School: </Text> 
-                          <Text style = {styles.school} >{item.school}</Text>
-  
-                          <Text>{'\n'}{'\n'}</Text>
-  
-                          <Text style = {styles.major_text} >   Major: </Text>
-                          <Text style = {styles.major} >{item.major}</Text>
-  
-                          <Text>{'\n'}{'\n'}</Text>
-  
-                          <Text style = {styles.gpa_text} >   GPA: </Text>
-                          <Text style = {styles.gpa} >{item.GPA}</Text> 
-  
-                          <Text>{'\n'}{'\n'}</Text>
-  
-                          <Text style = {styles.extra_info_text} >   Extra Information: </Text>
-                          <Text style = {styles.extra_info} >{item.information}</Text>
-  
-                          <Text>{'\n'}</Text>
-                      </Text>
-                  </View>
-  
-                  
-  
-              </View>
-          )}
-  
-        />  
-  
-      <View style = {styles.navs}>
-          <ButtonNavBar navigation={navigation}/>
-      </View> 
-      
-          </SafeAreaView>
-      </View>
-      
-    )
+                </Text>
+
+                
+                <Text style = {styles.personal_info_text}>Personal Info</Text>
+                
+
+                <View style = {styles.profile_border1} >
+                    <Text >
+                        <Text>{'\n'}</Text>
+                          
+                        <Text style = {styles.username_text} >   Full Name: </Text>
+                        <Text style = {styles.username} >{item.Name}</Text>
+
+
+                        <Text>{'\n'}{'\n'}</Text>
+
+                        <Text style = {styles.users_email_text}>   Email: </Text>
+                        <Text style = {styles.users_email} >{item.Email}</Text> 
+
+                        <Text>{'\n'}{'\n'}</Text>
+
+                        <Text style = {styles.school_text} >   School: </Text> 
+                        <Text style = {styles.school} >{item.school}</Text>
+
+
+                    </Text>
+                </View>
+
+                <Text style = {styles.about_me_text}>About Me</Text>
+
+                <View style = {styles.profile_border2} >
+                    <Text >
+                        <Text>{'\n'}</Text>
+
+                        <Text style = {styles.major_text} >   Major: </Text>
+                        <Text style = {styles.major} >{item.major}</Text>
+
+                        <Text>{'\n'}{'\n'}</Text>
+
+                        <Text style = {styles.gpa_text} >   GPA: </Text>
+                        <Text style = {styles.gpa} >{item.GPA}</Text> 
+
+                        <Text>{'\n'}{'\n'}</Text>
+
+                        <Text style = {styles.extra_info_text} >   Extra Information: </Text>
+                        <Text style = {styles.extra_info} >{item.information}</Text>
+
+                    </Text>
+                </View>
+
+                
+
+            </View>
+        )}
+
+      />  
+
+    <View style = {styles.navs}>
+        <ButtonNavBar navigation={navigation}/>
+    </View> 
+    
+        </SafeAreaView>
+    </View>
+    
+  )
 }
+
+
+ 
 
 export default SearchProfiles
