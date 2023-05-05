cc9dccd627548f2f5187c89730594414ced51bc0
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2023-05-04 22:21:09 -0400

    Update Friends.js

--- screens/Friends.js
+++ screens/Friends.js
@@ -1,83 +1,70 @@
-import { StyleSheet, Text, View, FlatList,  Image } from 'react-native'
+//Friends Page
 import React, { useState, useEffect } from 'react'
-import { query, collectionGroup, where, getDocs, doc, collection} from 'firebase/firestore';
+import { Text, View, FlatList } from 'react-native'
+import { query, collectionGroup, where, getDocs} from 'firebase/firestore';
 import { auth, db} from '../firebase';
 import { styles } from '../Style';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Divider } from 'react-native-elements';
 import TopHeaderBar from '../modules/TopHeaderBar';
 import ButtonNavBar from '../modules/NavBar';
 
-const Friends = ({route,navigation}) => {
-    const [users, setUsers] = useState([]);
-    const user = query (collectionGroup (db, 'friends'),where ("Email", "==", auth.currentUser?.email));
+const Friends = ({navigation}) => {
+  const [friend, setFriend] = useState([]);
+  const friends = query (collectionGroup (db, 'friends'),where ("Email", "==", auth.currentUser?.email));
 
-  
-  
-    useEffect(()=> {
-  
-    async function document(){
-  
-      const get_user = await getDocs(user) 
-  
-      const users = []
-     
-      get_user.forEach((doc) => {
-  
-        const { Email, Email2 } = doc.data()
-  
-        users.push ({
-          id: doc.id,
-          Email,
-          Email2,
-        })
-  
-      })
-  
-      setUsers(get_user.docs.map((doc) => ({ id: doc.id, ...doc.data()})))
-    }
-     
+  // Read appropriate friends collection into an array
+  const friend_doc = async () => {
+
+    const get_friend= await getDocs(friends) 
+
+    const friend = []
    
-    document()
-    },[])
-  
-    return (
-      <View style =  {styles.page}>
-  
-          <TopHeaderBar navigation={navigation}/>
+    get_friend.forEach((doc) => {
 
+      const { Email, Email2 } = doc.data()
+      friend.push ({
+        id: doc.id,
+        Email,
+        Email2,
+      })
+
+    })
+
+    setFriend(get_friend.docs.map((doc) => ({ id: doc.id, ...doc.data()})))
+  }
+  //Call the function document
+  useEffect(()=> {
+    friend_doc()
+  },[])
+  //return appropriate stylings and information
+  return (
+    <View style =  {styles.page}>
+      <TopHeaderBar navigation={navigation}/>
           <View style = {styles.profile_top_text_border}>
             <Text style = {styles.profile_top_text}>
               Friend List
             </Text>
           </View>
   
-          <SafeAreaView style = {styles.flatlist}>
+        <SafeAreaView style = {styles.flatlist}>
   
-         <FlatList
-  
-          data = {users}
-          renderItem = {({item}) => (
-              <View>
-             
+          <FlatList
+            data = {friend}
+            renderItem = {({item}) => (
+            <View>
              <Text style={styles.create_post}>{item.Email2}</Text>
-             
-              
-                  
-                 </View>
-          )}
+            </View>
+            )}
+          />  
   
-        />  
-  
-      <View style = {styles.navs}>
+        <View style = {styles.navs}>
           <ButtonNavBar navigation={navigation}/>
-      </View> 
+        </View> 
       
-          </SafeAreaView>
+        </SafeAreaView>
       </View>
       
     )
-  
 }
 
 export default Friends
