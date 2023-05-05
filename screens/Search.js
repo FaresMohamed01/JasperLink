89103d57a4aacf388cd994c7580726a23521bd6a
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2023-05-04 22:25:47 -0400

    Update Search.js

--- screens/Search.js
+++ screens/Search.js
@@ -1,20 +1,24 @@
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
+//Search Page
 import React from 'react'
+import { View, SafeAreaView } from 'react-native'
 import algoliasearch from 'algoliasearch';
 import { InstantSearch } from 'react-instantsearch-native';
 import SearchBox from './SearchBox';
 import { styles } from '../Style';
 import RefinementList from './RefinementList';
 import InfiniteHits from './InfiniteHits';
-import ButtonNavBar from '../modules/ButtonNavBar'
+import ButtonNavBar from '../modules/NavBar'
 import TopHeaderBar from '../modules/TopHeaderBar';
+
 //Configure the algoliasearch settings using our API key and APP ID
 const searchClient = algoliasearch(
   "9YLY0N9A8X",
   "8cd333f7b3ef8b8212c6ff67cd33c3ec"
 )
 
+//Implement all pages to search with Algolia
 const Search = ({navigation}) => {
+
   return (
     <View style = {styles.page}>
 
@@ -25,14 +29,14 @@ const Search = ({navigation}) => {
       <SafeAreaView style = {styles.flatlist}>
 
       {/*InstantSearch provided by Algolia to make the search happen. Calls the indexname that we called "users"*/}
-      <InstantSearch 
-        searchClient={searchClient}
-        indexName="users"
-      >
-         {/*Calls our search box, refinement list, infinitehits"*/}
-      
+       <InstantSearch 
+          searchClient={searchClient}
+          indexName="users"
+        >
+         
+      {/*Calls our search box, refinement list, infinitehits"*/}
       <View style = {styles.search_bar}>
-      <SearchBox />
+        <SearchBox />
       </View>
       
       <View>
@@ -44,16 +48,15 @@ const Search = ({navigation}) => {
       
       
       
-      </InstantSearch>
+       </InstantSearch>
 
         <View style = {styles.navs}>
           <ButtonNavBar navigation={navigation}/>
         </View> 
 
-        </SafeAreaView>
+      </SafeAreaView>
 
     </View>
-
   )
 }
 
