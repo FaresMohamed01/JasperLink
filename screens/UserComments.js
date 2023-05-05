eca4e1c5328ad06490aa84f89646f24c587cc569
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2023-05-04 22:28:04 -0400

    Update UserComments.js

--- screens/UserComments.js
+++ screens/UserComments.js
@@ -1,37 +1,32 @@
+//Comments page for users
 import React, { useState, useEffect }  from 'react';
-import {Pressable, FlatList, Text, Video, View, Image, ActivityIndicator, RefreshControl} from 'react-native';
-import {db, auth} from '../firebase';
-import {collection, getDocs, where, query, collectionGroup, orderBy} from 'firebase/firestore';
+import {Pressable, FlatList, Text, View, Image, ActivityIndicator, RefreshControl} from 'react-native';
+import {db} from '../firebase';
+import {getDocs, query, collectionGroup} from 'firebase/firestore';
 import ButtonNavBar from '../modules/NavBar';
-import TopBanner from '../modules/TopBanner';
-import PostCard from '../modules/PostCard';
 import TopHeaderBar from '../modules/TopHeaderBar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from '../Style';
 
 const UserComments = ({navigation}) => {
-    //Posts Array and Post query 
-    const [posts, setPosts] = useState([]);
-    const posts_query = query (collectionGroup (db, 'comments'));
+    //Comments Array and Comments query 
+    const [comment, setComment] = useState([]);
+    const comments_query = query (collectionGroup (db, 'comments'));
   
     //Refresh the page
     const [refresh, setRefresh] = useState(true);
-  
-    useEffect(()=> {
-      fetchData()
-    },[]) 
-  
+
+    //function to fetch comments
     const fetchData = async() => {
-        //function to fetch posts
-          const posts = []
-          const snapshot = await getDocs(posts_query) 
+          const comment = []
+          const snapshot = await getDocs(comments_query) 
   
           setRefresh(false);
       
-          posts.forEach((doc) => {
+          comment.forEach((doc) => {
             const {Email, image, post, Username} = doc.data()
           
-            posts.push ({
+            comment.push ({
               id: doc.id,
               Email,
               image,
@@ -40,22 +35,24 @@ const UserComments = ({navigation}) => {
             })
           })
       
-          setPosts(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data()})))
+          setComment(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data()})))
     }
-  
+
+    useEffect(()=> {
+      fetchData()
+    },[]) 
   
     return (
       <View  style = {styles.page}>
         <View>
           <TopHeaderBar navigation={navigation}/>
         </View>
-  
-  
-         {refresh ? <ActivityIndicator/> : null}
+
+        {refresh ? <ActivityIndicator/> : null}
   
         <SafeAreaView style = {styles.flatlist}>
         <FlatList 
-          data = {posts}
+          data = {comment}
           renderItem = {({item}) => (
             <Pressable>
               <View style = {styles.User_Comments_border}>
@@ -93,16 +90,17 @@ const UserComments = ({navigation}) => {
        )}  
        refreshControl={
        <RefreshControl refreshing={refresh} onRefresh={fetchData} />
-       }
-      />
+        }
+        />
   
         <View style = {styles.nav}>
           <ButtonNavBar navigation={navigation}/>
         </View>  
+
         </SafeAreaView>   
       </View>  
     );
-  
+    
   };
 
 
