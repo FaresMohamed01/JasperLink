b4430db9f70fac81e5339f8328a15082df5b38eb
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2023-05-04 22:22:17 -0400

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,8 @@
 //Home Screen Page
 import React, { useState, useEffect }  from 'react';
-import {Pressable, FlatList, Text, Video, View, Image, ActivityIndicator, RefreshControl} from 'react-native';
-import {db, auth} from '../firebase';
-import {collection, getDocs, where, query, collectionGroup, orderBy, doc} from 'firebase/firestore';
+import {Pressable, FlatList, Text, View, Image, ActivityIndicator, RefreshControl} from 'react-native';
+import {db} from '../firebase';
+import {getDocs, query, collectionGroup, orderBy} from 'firebase/firestore';
 import ButtonNavBar from '../modules/NavBar';
 import TopBanner from '../modules/TopBanner';
 import PostCard from '../modules/PostCard';
@@ -16,35 +16,34 @@ const HomeScreen = ({navigation}) => {
   const [posts, setPosts] = useState([]);
   const posts_query = query (collectionGroup (db, 'posts'), orderBy("timestamp", "desc"));
 
-  
   //Refresh the page
   const [refresh, setRefresh] = useState(true);
+
+  const fetchData = async() => {
+    //function to fetch posts
+      const posts = []
+      const snapshot = await getDocs(posts_query) 
+
+      setRefresh(false);
+  
+      posts.forEach((doc) => {
+        const {Email, image, post,users} = doc.data()
+      
+        posts.push ({
+          id: doc.id,
+          Email,
+          image,
+          post,
+          users,
+        })
+      })
+  
+      setPosts(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data()})))
+}
 
   useEffect(()=> {
     fetchData()
   },[]) 
-
-  const fetchData = async() => {
-      //function to fetch posts
-        const posts = []
-        const snapshot = await getDocs(posts_query) 
-
-        setRefresh(false);
-    
-        posts.forEach((doc) => {
-          const {Email, image, post,users} = doc.data()
-        
-          posts.push ({
-            id: doc.id,
-            Email,
-            image,
-            post,
-            users,
-          })
-        })
-    
-        setPosts(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data()})))
-  }
 
 
   return (
