ae0c8cfd9d5aa19f3d07e8e0afc6301e279fd129
Fares Mohamed <61517859+FaresMohamed01@users.noreply.github.com>
2023-05-04 22:22:56 -0400

    Update InfiniteHits.js

--- screens/InfiniteHits.js
+++ screens/InfiniteHits.js
@@ -1,20 +1,16 @@
+//Infinite Hits Page
 import React from 'react';
-import { View, FlatList, TouchableOpacity, Text, Button} from 'react-native';
+import { View, FlatList, TouchableOpacity, Text} from 'react-native';
 import PropTypes from 'prop-types';
 import { connectInfiniteHits } from 'react-instantsearch-native';
 import { styles } from '../Style';
 import { useNavigation } from '@react-navigation/native';
-import { db, firebase, auth } from '../firebase';
-import { where } from 'firebase/firestore';
-
 
 const InfiniteHits = ({ hits, hasMore, refineNext, navigation}) => {
-    
-      navigation = useNavigation()
+    navigation = useNavigation()
     return (
       //Create A FlatList with all information. item.objectID is fetched from the Algolia database.
       // Afterwards, the username are displayed in the search page and each username are pressable to the SearchProfiles.js
-      
       <FlatList
             data={hits}
             keyExtractor={item => item.objectID}
@@ -22,26 +18,25 @@ const InfiniteHits = ({ hits, hasMore, refineNext, navigation}) => {
             onEndReached={() => hasMore && refineNext()}
             renderItem={({item}) => (
                 <View style={styles.item}>
-            <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('SearchProfiles', {
-              itemId: item.Name,
-              itemEmail: item.Email,
-            });
-          }}
-             >      
-       <View style = {styles.filtered_border}>
-        <Text style = {styles.filtered_names}>{(item.Name)}</Text> 
-        <Text style = {styles.filtered_emails}>{item.Email}</Text> 
-       </View>
-       </TouchableOpacity>
-        </View>
+                  
+                    <TouchableOpacity
+                      onPress={() => {
+                        navigation.navigate('SearchProfiles', {
+                          itemId: item.Name,
+                          itemEmail: item.Email,
+                        });
+                      }}
+                    >      
+       
+                      <View style = {styles.filtered_border}>
+                        <Text style = {styles.filtered_names}>{(item.Name)}</Text> 
+                        <Text style = {styles.filtered_emails}>{item.Email}</Text> 
+                     </View>
+                    </TouchableOpacity>
+                </View>
             )}
         />
-
     )
-    
-    
 }
 //PropTypes for hits, hasmore, refinenext.
 InfiniteHits.propTypes = {
